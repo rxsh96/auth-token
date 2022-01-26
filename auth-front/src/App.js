d8202796bce9a8588ac41a9107bfd414b494984e
Ricardo Serrano <rxsh964@gmail.com>
2022-01-26 03:07:34 -0500

    frontend base design

--- auth-front/src/App.js
+++ auth-front/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./components/logo/Logo";
+import PopUp from "./components/popup/PopUp";
+import UserForm from "./components/userform/UserForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <UserForm />
+      <PopUp />
     </div>
   );
 }
---
+++ auth-front/src/components/userform/UserForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./UserForm.css";
+
+const UserForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div className="ma4 mt0">
+      <p className="black b">{"Generate Authentication Key"}</p>
+      <div className="center">
+        <div className="form pa4 br2 shadow-5">
+          <input
+            className="f4 pa2 w-10 center"
+            type="text"
+            placeholder="username"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-10 grow f4 link ph3 pv2 dib black my-color"
+            onClick={onButtonSubmit}
+          >
+            {" "}
+            Generate
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserForm;
